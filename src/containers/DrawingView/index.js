8254bcc29f3e2cb4cbcc350b36c23b5e490756c1
anhtuannguyen <tuananh871995@gmail.com>
2021-08-14 21:19:37 +0700

    Handle select element and move

--- src/containers/DrawingView/index.js
+++ src/containers/DrawingView/index.js
@@ -9,17 +9,46 @@ const DRAW_LINE = 'DRAW_LINE';
 const DRAW_RECTANGLE = 'DRAW_RECTANGLE';
 const SELECT = 'SELECT';
 
-const createElement = (x1, y1, x2, y2, type) => {
-    const roughElement = type === DRAW_LINE
+const NONE_ACTION = 'NONE_ACTION';
+const DRAWING = 'DRAWING';
+const MOVING = 'MOVING';
+
+const createElement = (id, x1, y1, x2, y2, tool) => {
+    const roughElement = tool === DRAW_LINE
       ? generator.line(x1, y1, x2, y2)
       : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-  return { x1, y1, x2, y2, roughElement }
+  return { id, x1, y1, x2, y2, tool, roughElement }
+};
+
+const isWithinElement = (x, y, element) => {
+  const { tool, x1, x2, y1, y2 } = element;
+  if (tool === DRAW_RECTANGLE) {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return x >= minX && x <= maxX && y >= minY && y <= maxY;
+  }
+  if (tool === DRAW_LINE) {
+    const pointA = { x: x1, y: y1 };
+    const pointB = { x: x2, y: y2 };
+    const pointC = { x, y };
+    const offset = distance(pointA, pointB) - (distance(pointA, pointC) + distance(pointB, pointC));
+    return Math.abs(offset) < 1;
+  }
+};
+
+const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
+const getElementByPosition = (x, y, elements) => {
+  return elements.find(ele => isWithinElement(x, y, ele));
 };
 
 const DrawingView = () => {
   const [elements, setElements] = useState([]);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [toolAction, setToolAction] = useState(DRAW_LINE);
+  const [action, setAction] = useState(NONE_ACTION);
+  const [tool, setTool] = useState(DRAW_LINE);
+  const [selectedElement, setSelectedElement] = useState(null);
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("drawing-canvas");
@@ -31,52 +60,77 @@ const DrawingView = () => {
     elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
   }, [elements]);
 
-  const handleMouseMove = (e) => {
-    if(!isDrawing) {
-      return;
-    }
-
-    const { clientX, clientY } = e;
-    const index = elements.length - 1;
-    const { x1, y1 } = elements[index];
-    const updatedElement = createElement(x1, y1, clientX, clientY, toolAction);
+  const updateElement = (id, x1, y1, clientX, clientY, tool) => {
+    const updatedElement = createElement(id, x1, y1, clientX, clientY, tool);
 
     const newElements = [...elements];
-    newElements[index] = updatedElement;
+    newElements[id] = updatedElement;
     setElements(newElements);
   };
 
-  const handleMouseDown = (e) => {
-    setIsDrawing(true);
-
+  const handleMouseMove = (e) => {
     const { clientX, clientY } = e;
-    const element = createElement(clientX, clientY, clientX, clientY, toolAction);
-    setElements(prevState => [...prevState, element]);
+
+    if(tool === SELECT) {
+      e.target.style.cursor = getElementByPosition(clientX, clientY, elements) ? 'move' : 'default';
+    }
+
+    if (action === DRAWING) {
+      const index = elements.length - 1;
+      const { x1, y1 } = elements[index];
+      updateElement(index, x1, y1, clientX, clientY, tool);
+    } else if (action === MOVING) {
+      const { id, x1, x2, y1, y2, tool, offsetX, offsetY } = selectedElement;
+      const width = x2 - x1;
+      const height = y2 - y1;
+      const newX = clientX - offsetX;
+      const newY = clientY - offsetY;
+      updateElement(id, newX, newY, newX + width, newY + height, tool);
+    }
+  };
+
+  const handleMouseDown = (e) => {
+    const { clientX, clientY } = e;
+    if (tool === SELECT) {
+      const element = getElementByPosition(clientX, clientY, elements);
+      if (element) {
+        const offsetX = clientX - element.x1;
+        const offsetY = clientY - element.y1;
+        setSelectedElement({ ...element, offsetX, offsetY });
+        setAction(MOVING);
+      }
+    } else {
+      setAction(DRAWING);
+      const id = elements.length;
+      const element = createElement(id, clientX, clientY, clientX, clientY, tool);
+      setElements(prevState => [...prevState, element]);
+    }
   };
 
   const handleMouseUp = () => {
-    setIsDrawing(false);
+    setAction(NONE_ACTION);
+    setSelectedElement(null);
   };
 
   return (<div className="drawing-view-container">
     <div className="drawing-menu-bar">
       <Button
-        onClick={() => {setToolAction(SELECT)}}
+        onClick={() => {setTool(SELECT)}}
         text={'Select'}
         title={'Select an element and move'}
-        isSelected={toolAction === SELECT}
+        isSelected={tool === SELECT}
       />
       <Button
-        onClick={() => {setToolAction(DRAW_LINE)}}
+        onClick={() => {setTool(DRAW_LINE)}}
         text={'Line'}
         title={'Draw a line element, you can resize this element'}
-        isSelected={toolAction === DRAW_LINE}
+        isSelected={tool === DRAW_LINE}
       />
       <Button
-        onClick={() => {setToolAction(DRAW_RECTANGLE)}}
+        onClick={() => {setTool(DRAW_RECTANGLE)}}
         text={'Rectangle'}
         title={'Draw a rectangle element, you can resize this element'}
-        isSelected={toolAction === DRAW_RECTANGLE}
+        isSelected={tool === DRAW_RECTANGLE}
       />
       <Button onClick={() => {}} text={'Undo'}/>
       <Button onClick={() => {}} text={'Re-undo'}/>
